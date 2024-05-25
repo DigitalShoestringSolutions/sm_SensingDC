0b79de0af4ab99cee3fe354edc1f94bb58777443
Toby Harris <51968582+tobyaharris@users.noreply.github.com>
2024-05-25 15:16:16 +0100

    Private vars and comments in QuadratureEncoder.py

--- code/hardware/generic/QuadratureEncoder.py
+++ code/hardware/generic/QuadratureEncoder.py
@@ -1,6 +1,6 @@
 # QuadratureEncoder.py
 
-# imports
+# installed imports
 from gpiozero import RotaryEncoder
 
 
@@ -9,19 +9,28 @@ class EncoderWheel():
     def __init__(self, Apin, Bpin, steps_per_rev, circ=None, dia=None):
         """Uses gpiozero.RotaryEncoder. Specify either circ or dia."""
         self.encoder = RotaryEncoder(Apin, Bpin, max_steps=0)
-        self.steps_per_rev = steps_per_rev
+        self._steps_per_rev = steps_per_rev
 
         if circ is None:
             if dia is None:
                 raise TypeError("Either wheel circumference or diameter needs to be specified!")
             else:
-                self.circ = 3.14159265359*dia
+                self._circ = 3.14159265359*dia
         else:
-            self.circ = circ
+            self._circ = circ
 
-    def __call__(self):
-        """Return the current displacement of the wheel, in the units of circ or dia supplied."""
-        dist = self.circ*self.encoder.steps/self.steps_per_rev
-        return round(dist,3)
+    def __call__(self, rounding=3):
+        return self.get_current_position(rounding)
 
-    # position can be reset with .encoder.steps = 0 , no wrapper needed.
+    def get_current_position(self, rounding=None):
+        """Return the current tangential displacement of the wheel, in the units of circ or dia supplied.
+        rounding should be either None or an int of decimal places.
+        Position can be reset with .encoder.steps = 0, no wrapper needed
+        """
+
+        dist = self._circ*self.encoder.steps/self._steps_per_rev
+
+        if rounding is None:
+            return dist
+        else:
+            return round(dist, rounding)
