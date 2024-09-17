b7b7a7c281e4b270cd252e95dc6b71acea9398b6
Toby Harris <51968582+tobyaharris@users.noreply.github.com>
2024-09-17 20:17:03 +0100

    Logging

--- code/utilities/mqtt_out.py
+++ code/utilities/mqtt_out.py
@@ -9,6 +9,7 @@ Public API: only the function 'publish'
 """
 # standard imports
 import json
+import logging
 
 # installed imports
 import paho.mqtt.client as pahomqttclient
@@ -24,17 +25,19 @@ default_port = 1883
 
 
 # startup
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 client = pahomqttclient.Client()
 client._is_connected = False    # create custom attribute
 
 def _connect(broker, port):
-    print("MQTT client connecting to broker", broker, "port", port)
+    logger.info("MQTT client connecting to broker", broker, "port", port, "...")
     client.connect(broker, port)
     client._is_connected = True # should test connection attempt was sucessful
-
+    logger.info("MQTT client is connected to broker", broker, "port", port)
 
 def publish(msg, topic=default_topic, broker=default_broker, port=default_port):
-    """Add timestamp to msg (if not provided), connect (if not already) and publish. 
+    """Add timestamp to msg (if not provided), connect (if not already) and publish.
     Arg reordering is deliberate to allow kwargs.
     """
 
@@ -43,19 +46,22 @@ def publish(msg, topic=default_topic, broker=default_broker, port=default_port):
 
     # Ensure the mqtt client is ready to publish
     if not client._is_connected:
+        logger.info("Attempting to publish MQTT message without connection to broker - connecting now")
         _connect(broker, port)
 
     # format the message
     if type(msg) is dict:                               # preferred
         # The below ordering allows the user to specify their own timestamp in the message dict if desired.
         # If an entry with key "timestamp" is not provided, one will be added using time of publication.
-        # json.dumps(mydict) returns a string which is very similar to the output of str(mydict), 
-        #   but crucially with json.dumps() strings have double quotes as required by the json spec, 
+        # json.dumps(mydict) returns a string which is very similar to the output of str(mydict),
+        #   but crucially with json.dumps() strings have double quotes as required by the json spec,
         #   while str(mydict) gives single quotes and is not recognised as json.
         payload = json.dumps({'timestamp': timestamp} | msg)
-        
+
     else:                                               # failover
         payload = "timestamp: " + timestamp + " " + str(msg)
 
     # publish to mqtt
+    logger.debug("publishing to topic:", topic, "broker:", broker, "port:", port, "the following", type(payload), ":")
+    logger.info(payload)
     client.publish(topic, payload)
--- code/utilities/timestamp.py
+++ code/utilities/timestamp.py
@@ -1,12 +1,19 @@
 # timestamp.py
 
-# do we need to use both of these together?
+# standard imports
+import logging
+# do we need to use both of the following together?
 import time
 import datetime
 
+#startup
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 def get_timestamp():
     """Get a string containing the current lcoal time in ISO8601 format"""
     __dt = -1 * (time.timezone if (time.localtime().tm_isdst == 0) else time.altzone)
     tz = datetime.timezone(datetime.timedelta(seconds=__dt))
-    return datetime.datetime.now(tz=tz).isoformat()
+    timestamp = datetime.datetime.now(tz=tz).isoformat()
+    logger.debug("retrieved current timestamp as", timestamp)
+    return timestamp
