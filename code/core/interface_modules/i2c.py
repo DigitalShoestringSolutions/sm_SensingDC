93a2a2650c4d8314572ee48e7828cd1b58345d4e
tobyaharris <51968582+tobyaharris@users.noreply.github.com>
2025-10-09 12:27:08 +0100

    v1.2.0 release (#25)
    * Feature/i2c multibyte addrs (#23)
    
    * SEN55 air particulate sensor support (#24)

--- code/core/interface_modules/i2c.py
+++ code/core/interface_modules/i2c.py
@@ -17,20 +17,22 @@ class I2C:
     def initialise(self):
         self.i2c.open(self.bus)
 
-    def read_register(self,address,register,num_bytes,stop=False, delay:float=None):     # Write address, register_no  -- Read address, data
+
+    def read_register(self, device_address, register, num_bytes, stop=False, delay:float=None) -> list:
         """Read a number of registers from attached I2C device.
 
         :param int address:   I2C slave device address.
-        :param int register:  Memory register to read first data byte from.
+        :param register:      Memory register to read first data byte from. Single int <= 255 or list of ints each <= 255.
         :param int num_bytes: Number of sequential bytes to read.
         :paran bool stop:     (optional) If false, complete the reg write and data read in a single transaction.
-        :param float delay:   (optional) Seconds to sleep for between writing register and reading back data. Requires `stop=True` to be effective.
+        :param float delay:   (optional) Seconds to sleep for between writing register and reading back data.
         """
+        if isinstance(register, int): # accept single-byte addresses as int, or multi-byte addresses as list only
+            register = [register]
+        write_reg_addr = Msg.write(device_address, register)
+        read_reg_data = Msg.read(device_address, num_bytes)
 
-        write_reg_addr = Msg.write(address,[register])
-        read_reg_data = Msg.read(address,num_bytes)
-
-        if stop:
+        if stop or (delay is not None): # allow supplying delay as a kwarg while not supplying stop
             self.i2c.i2c_rdwr(write_reg_addr)
             if delay:
                 time.sleep(delay)
@@ -40,6 +42,39 @@ class I2C:
 
         return list(read_reg_data)
 
-    def write_register(self,address,register,data):    #Write address, register, data...
-        write_reg_addr_data = Msg.write(address,[register,*data])
-        self.i2c.i2c_rdwr(write_reg_addr_data)
+
+    def read(self, device_address:int, num_bytes:int) -> list:
+        """Read bytes from a device on the I2C bus without specifying a memory register
+
+        :param int device_address: I2C slave device address.
+        :param int num_bytes: Number of bytes to read from device
+        """
+        read_reg_data = Msg.read(device_address, num_bytes)
+        self.i2c.i2c_rdwr(read_reg_data)
+        return list(read_reg_data)
+
+
+    def write(self, device_address:int, data):
+        """Write bytes to a device on the I2C bus without specifying a memory register
+
+        :param int device_address: I2C slave device address.
+        :param data: Single int <= 255 or list of ints each <= 255 to write to device.
+        """
+        if isinstance(data, int): # accept single-byte data as int, or multi-byte data as list only
+            data = [data]
+        write_data = Msg.write(device_address, data)
+        self.i2c.i2c_rdwr(write_data)
+
+
+    def write_register(self, device_address:int, register:int, data):
+        """Set memory address `register` on an I2C slave device to `data`.
+
+        :param int device_address: I2C slave device address.
+        :param register: Memory register to write to. Single int <= 255 or list of ints each <= 255.
+        :param data: Single int <= 255 or list of ints each <= 255 to write to device memory.
+        """
+        if isinstance(register, int): # accept single-byte addresses as int, or multi-byte addresses as list only
+            register = [register]
+        if isinstance(data, int): # accept single-byte data as int, or multi-byte data as list only
+            data = [data]
+        self.write(device_address, [*register, *data])
--- code/core/interface_modules/spi.py
+++ code/core/interface_modules/spi.py
@@ -18,28 +18,31 @@ class SPI:
         self.spi.max_speed_hz  = self.default_speed if speed is None else speed
         return self.spi.xfer2(bytes)
 
-    def read(self, reg: int, n_words: int=1, mode: int=None, speed_hz: int=None) -> list:
+    def read(self, reg, n_words: int=1, mode: int=None, speed_hz: int=None) -> list:
         """Clocks `reg` into the SPI device, then clock out `n_words` more words. 
         
         Unless bits per word has been altered, words typically equate to bytes.
 
-        :param int reg:     The register to be clocked into the device before reading.
+        :param reg:         The register to be clocked into the device before reading. Single int or list of ints.
         :param int n_words: (optional) The number of words to read. Default 1.
         :param int mode:    (optional) Change the bus config before the transaction. `(clock polarity << 1 | clock phase)` as a 2 bit int.
         :param int speed:   (optional) Change the bus config before the transaction. Set SPI clock speed in Hz.
         :return:            list of integers        
         """
-        return self.transfer([reg] + [0]*n_words, mode=mode, speed=speed_hz)[1:] # The first word returned is discarded, as it was while the reg was being clocked in.
+        if isinstance(reg, int):
+            reg = [reg]
+        return self.transfer(reg + [0]*n_words, mode=mode, speed=speed_hz)[1:] # The first word returned is discarded, as it was while the reg was being clocked in.
 
-    def write(self, reg: int, data, mode: int=None, speed_hz: int=None) -> None:
-        """Write `reg` to the SPI device, then write `data` to the SPI device. `data` can be an int for a single word/byte, or a list of ints for multiple words/bytes.
+    def write(self, reg, data, mode: int=None, speed_hz: int=None) -> None:
+        """Write `reg` to the SPI device, then write `data` to the SPI device. Either can be an int for a single word/byte, or a list of ints for multiple words/bytes.
 
-        :param int reg:   The register to be clocked into the device before writing `data`.
+        :param reg:       The register to be clocked into the device before writing `data`. Single int or list of ints.
         :param data:      The words to be written after `reg`. Single int or list of ints.
         :param int mode:  (optional) Change the bus config before the transaction. `(clock polarity << 1 | clock phase)` as a 2 bit int.
         :param int speed: (optional) Change the bus config before the transaction. Set SPI clock speed in Hz.
         """
+        if isinstance(reg, int):
+            reg = [reg]
         if isinstance(data, int):
             data = [data]
-        self.transfer([reg] + data, mode=mode, speed=speed_hz)
-
+        self.transfer(reg + data, mode=mode, speed=speed_hz)
