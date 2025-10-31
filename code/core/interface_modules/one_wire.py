f3fd361177c890b3ee096e72a1f6a3e224ecf4d0
Toby Harris <51968582+tobyaharris@users.noreply.github.com>
2025-10-31 16:27:07 +0000

    OneWire accepts throwaway config, has required initialise method

--- code/core/interface_modules/one_wire.py
+++ code/core/interface_modules/one_wire.py
@@ -5,13 +5,19 @@ logger = logging.getLogger(__name__)
 
 class OneWire:
 
-    def __init__(self):  # supports GPIO4 only!
+    def __init__(self, config={}):  # :param config not used but must be handled
+        # supports GPIO4 only!
         # would be nice to support other pins, or be able to apply a software pull-up.
-        self.devices_dir = "/sys/bus/w1/devices/" # dtoverlay for pin4
+        self.devices_dir = "/sys/bus/w1/devices/"  # dtoverlay for pin4
 
-        if not os.path.exists(self.self.devices_dir):
-            logger.error(f"One wire device not found at {self.self.devices_dir}")
-        
+
+    def initialise(self):
+        if os.path.exists(self.devices_dir):
+            logger.debug(f"One wire bus found at {self.devices_dir}")
+        else:
+            logger.error(f"One wire bus not found at {self.devices_dir}")
+
+
     def get_devices_on_bus(self) -> list:
         # Detect devices
         devices = os.listdir(self.devices_dir)
@@ -24,8 +30,10 @@ class OneWire:
 
         return devices
 
+
     def _get_filepath(self, id:str) -> str:
         return self.devices_dir + id + "/w1_slave"
+
 
     def read_file(self, id):
         with open(self._get_filepath(id), 'r') as f:
